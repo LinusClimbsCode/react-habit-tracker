3fb33d48377704c4565425a169bd77179154b1a0
Linus Mühlpointner <l.muehlpointner@web.de>
2025-06-04 11:13:18 +0200

    finished habitListItems func

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import Header from './components/Header'
+import HabitList from './components/HabitList'
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      content: 'Learn programming',
+      goals: 3,
+    },
+  ]);
+
+  const onDelete = (itemID) => {
+    setItems(items.filter(item => item.id !== itemID));
+  }
 
   return (
-    <div className='bg-indigo-200'>
+    <div className='bg-slate-800 text-neutral-100'>
     <Header />
-    
+    <HabitList items={items} setItems={setItems} onDelete={onDelete} />
     </div>
   )
 }
--- src/components/HabitListItem.jsx
+++ src/components/HabitListItem.jsx
@@ -1,5 +1,46 @@
-const HabitFormItem = () => {
-    return undefined
-}
+import { useState } from 'react';
 
-export default HabitFormItem
+const HabitFormItem = ({ item, onDelete}) => {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = (operator) => {
+    if (operator === '+') {
+      setCounter((prev) => prev + 1);
+    } else {
+      setCounter((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-between bg-slate-500 rounded p-2 m-4">
+      <p>{item.content}</p>
+      <div className="flex items-center justify-center">
+        <button
+          className="flex items-center justify-center bg-green-500 rounded p-2 m-2 min-w-10 min-h-10"
+          onClick={() => handleClick('+')}
+          disabled={(counter == item.goals) ? true : false}
+        >
+          +
+        </button>
+        <button
+          className="flex items-center justify-center bg-slate-800 rounded p-2 m-2 min-w-10 min-h-10"
+          onClick={() => handleClick('-')}
+          disabled={(counter == 0) ? true : false}
+        >
+          -
+        </button>
+        <p className="flex">
+          progress: {counter}/{item.goals}
+        </p>
+        <button
+            className="flex items-center justify-center bg-red-500 rounded p-2 m-2 min-w-10 min-h-10 text-white"
+            onClick={() => onDelete(item.id)}
+        >
+            Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default HabitFormItem;
